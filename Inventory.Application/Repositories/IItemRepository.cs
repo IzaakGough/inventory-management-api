1b3945b181e7365de143f400c160df20f126f8da
IzaakGough <izaakgough@gmail.com>
2026-01-11 16:46:02 +0000

    Add item repository interface

---
+++ Inventory.Application/Repositories/IItemRepository.cs
@@ -0,0 +1,19 @@
+﻿using Inventory.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory.Application.Repositories
+{
+    public interface IItemRepository
+    {
+        Task<Item?> GetByIdAsync(Guid id, CancellationToken ct);
+        Task<IReadOnlyList<Item>> GetAllAsync (CancellationToken ct);
+        Task<Item> CreateAsync(Item item, CancellationToken ct);
+        Task<bool> UpdateAsync(Item item, CancellationToken ct);
+        Task<bool> DeleteAsync(Guid id, CancellationToken ct);
+        
+    }
+}
