ee1f8adfa8bae125d609d3fec1e26f4b26b81b63
IzaakGough <izaakgough@gmail.com>
2026-01-11 16:29:37 +0000

    Add Item Domain Entity

---
+++ Inventory.Domain/Entities/Item.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory.Domain.Entities
+{
+    public class Item
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        public string Sku { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+
+        public int Quantity { get; set; }
+        public string? Location { get; set; }
+
+        public DateTimeOffset CreatedAt { get; set; } = DateTimeOffset.UtcNow;
+        public DateTimeOffset UpdatedAt { get; set; } = DateTimeOffset.UtcNow;
+
+
+    }
+}
